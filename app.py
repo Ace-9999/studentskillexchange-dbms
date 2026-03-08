1f85ce1b4c8a7ee87ae1343747b196cb21190a8e
Anirudh Reddy <anirudh.reddy6002@gmail.com>
2026-03-08 19:45:21 +0530

    modify app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import mysql.connector
+import os
 
 app = Flask(__name__)
 
@@ -173,5 +174,7 @@ def certifications(student_id):
 # RUN SERVER
 # =========================
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
