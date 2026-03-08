d81b6fde5f3d8f5d7ab1830f348a36624cc51ab8
Anirudh Reddy <anirudh.reddy6002@gmail.com>
2026-03-08 19:51:53 +0530

    edit app

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ app = Flask(__name__)
 # DATABASE CONNECTION
 # =========================
 
-import os
-
 db = mysql.connector.connect(
     host=os.environ.get("DB_HOST"),
+    port=os.environ.get("DB_PORT"),
     user=os.environ.get("DB_USER"),
     password=os.environ.get("DB_PASSWORD"),
     database=os.environ.get("DB_NAME")
